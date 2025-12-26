f17486c1e7ee2e03c0118a28be9ab9095d56fd43
Maxime <maxime.huart60@laposte.net>
2025-12-26 18:33:20 +0100

    attempts to compile on Linux

--- hooks/00_numpy_runtime_hook.py
+++ hooks/00_numpy_runtime_hook.py
@@ -1,3 +1,11 @@
+# This must run before Torch or anything else
+import builtins
+import importlib
+
+# Force NumPy C extensions to load
 import numpy
 import numpy.core._multiarray_umath
+
+# Some packages check builtins._NUMPY_AVAILABLE; ensure PyTorch sees NumPy
+builtins._NUMPY_AVAILABLE = True
 
